67c487237ddface7e2d8b5a8280e1d4111be7ac5
dleeftink <dennisleeftink@gmail.com>
2023-03-06 17:09:51 +1000

    1 file modified

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 const ax = require("axios");
 
-const test = await ax('https://api.ipify.org/?format=json');
+const test = ax('https://api.ipify.org/?format=json');
 
 export default async (req, res) => {
 
   let dat = Math.random()
   let temp = await fetch('https://api.ipify.org/?format=json').then(d=>d.json())
-  res.send({...temp,test,version,dat})
+  res.send({...temp,test:await test,version,dat})
   
 };
