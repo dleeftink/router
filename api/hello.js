61a5a37d5a0b9c33aa34a040d555052a95cf18cd
dleeftink <dennisleeftink@gmail.com>
2023-03-06 15:29:49 +1000

    1 file modified

--- api/hello.js
+++ api/hello.js
@@ -1,9 +1,9 @@
-import path from 'path';
+import VERSION from 'axios';
 
 export default async (req, res) => {
 
   let dat = Math.random()
   let temp = await fetch('https://api.ipify.org/?format=json').then(d=>d.json())
-  res.send({...temp,dat,path})
+  res.send({...temp,dat,VERSION})
   
 };
