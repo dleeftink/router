be22281ef1c004fd0f7d1fda9fde0546cef81441
dleeftink <dennisleeftink@gmail.com>
2023-03-06 15:08:09 +1000

    1 file modified

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,7 @@
 export default async (req, res) => {
 
+  let dat = Math.random()
   let temp = await fetch('https://api.ipify.org/?format=json').then(d=>d.json())
-  res.send({...temp})
+  res.send({...temp,dat})
   
 };
