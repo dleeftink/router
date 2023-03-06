309a29dfe74443dafa1896fb5d248f70f1089fe8
dleeftink <dennisleeftink@gmail.com>
2023-03-06 19:04:54 +1000

    1 file modified, 2 added, 1 deleted

--- api/hello.js
+++ api/hello.js
@@ -1,11 +1,11 @@
-import * as all from './utils.js';
+import * as all from 'src/utils.js';
 
 const axios = require('axios');
 const ax = axios.default;
 
 module.exports = async (req, res) => {
 
-    let temp = (await ax("https://api.ipify.org/?format=json")).data;
-    res.send({temp})
+  let temp = (await ax('https://api.ipify.org/?format=json')).data;
+  res.send({ temp });
 
 };
