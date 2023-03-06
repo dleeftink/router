4d9876e8244ac6a216ee853e32152339dbfa2836
dleeftink <dennisleeftink@gmail.com>
2023-03-06 16:05:25 +1000

    1 file modified

--- api/hello.js
+++ api/hello.js
@@ -1,9 +1,9 @@
-const axios = require('axios');
+// const axios = require('axios');
 
 export default async (req, res) => {
 
   let dat = Math.random()
   let temp = await fetch('https://api.ipify.org/?format=json').then(d=>d.json())
-  res.send({...temp,dat,VERSION:axios.VERSION})
+  res.send({...temp,dat,VERSION:1})
   
 };
