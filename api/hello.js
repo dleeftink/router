ec739ce130a564c1f57184c05f558b5d5857a44a
dleeftink <dennisleeftink@gmail.com>
2023-03-06 18:32:56 +1000

    1 file modified, 1 added

--- api/hello.js
+++ api/hello.js
@@ -1,23 +1,8 @@
+import allowCors from './src/utils.js';
+
 const axios = require('axios');
 
 const ax = axios.default;
-
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
-    }
-    return await fn(req, res)
-  }
 
 module.exports = async (req, res) => {
 
