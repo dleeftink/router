c10bc8e397999976da6c088eee66a5f5d8d60487
dleeftink <dennisleeftink@gmail.com>
2023-03-06 17:29:34 +1000

    2 files modified, 1 added

---
+++ api/screen.js
@@ -0,0 +1,16 @@
+import chrome from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
+
+export default async function screenshot(url: string) {
+  const options = {
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  };
+
+  const browser = await puppeteer.launch(options);
+  const page = await browser.newPage();
+  await page.setViewport({ width: 2000, height: 1000 });
+  await page.goto(url, { waitUntil: 'networkidle0' });
+  return await page.screenshot({ type: 'png' });
+}
