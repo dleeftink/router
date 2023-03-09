1691a0c26b47042c13697af6ed46ad55a6cdd4ef
dleeftink <dennisleeftink@gmail.com>
2023-03-09 12:17:27 +1000

    1 file modified

--- api/hello.js
+++ api/hello.js
@@ -2,26 +2,21 @@
 
 const { select } = require('../src/utils');
 const axios = require('axios');
-const httpProxy = require('http-proxy');
 const ax = axios.default;
 
-const proxy = httpProxy.createProxyServer({});
-
 module.exports = async (req, res) => {
+  let ipi = (await ax('https://api.ipify.org/?format=json')).data;
+  let geo = (await ax('http://ipwho.is')).data; ;
 
-  let ip = (await ax('https://api.ipify.org/?format=json')).data;
-  let url = 'http://ipwho.is';
+  let prox = (
+    await axios.get('https://api.ipify.org/?format=json', {
+      proxy: {
+        protocol: 'http',
+        host: '128.199.202.122',
+        port: 8080,
+      },
+    })
+  )?.data;
 
-  let newReq = new Request(url, req);
-
-  proxy.web(newReq, res, { target: `http://185.135.157.89:8080` })
-
-  proxy.on('proxyRes', function (proxyRes, rq, rs) {
-    res.send({...ip,proxyRes,rq,rs,d:Math.random()})
-    //console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2));
-  });
-   
-
-  //res.send({ ...ip, fn:select(1),oi:Math.random()});
-
+  res.send({ ipi, geo, prox, fn: select(1), d: Math.random() });
 };
